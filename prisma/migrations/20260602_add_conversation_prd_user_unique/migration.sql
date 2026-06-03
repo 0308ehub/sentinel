b6de9b562c3abe263f3a37a91699e2d52ee11822
Alan Wei <awei001@berkeley.edu>
2026-06-02 19:52:15 -0700

    chore: remove broken partial migration files

--- prisma/migrations/20260602_add_conversation_prd_user_unique/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_prdId_userId_key" UNIQUE("prdId", "userId");
--- prisma/migrations/20260602_add_prd_conversation_link/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Conversation" ADD COLUMN "prdId" TEXT;
-
--- CreateIndex
-CREATE INDEX "Conversation_prdId_idx" ON "Conversation"("prdId");
-
--- AddForeignKey
-ALTER TABLE "Conversation" ADD CONSTRAINT "Conversation_prdId_fkey" FOREIGN KEY ("prdId") REFERENCES "PRD"("id") ON DELETE CASCADE ON UPDATE CASCADE;
